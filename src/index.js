a732dccf843b678e4b4d1c10810dda3bda132015
Patryk Bialek <p.bialek@kaft.pl>
2020-11-14 18:06:12 +0100

    change select lang title

--- src/index.js
+++ src/index.js
@@ -1,9 +1,1 @@
 // import '../assets/css/styles.css';
-
-import * as $ from "jquery";
-
-$("#language").on("shown.bs.modal", function () {
-  // $('#myInput').trigger('focus')
-});
-
-console.log($("#language"))
