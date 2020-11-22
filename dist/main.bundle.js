47120e1ca7cb65de2d3a8cf1c0ef874483f139ab
Patryk Bialek <p.bialek@kaft.pl>
2020-11-22 12:59:33 +0100

    implement park category filters

--- dist/main.bundle.js
+++ dist/main.bundle.js
@@ -107,6 +107,19 @@ __webpack_require__.r(__webpack_exports__);
 /***/ (function(module, exports) {
 
 // import '../assets/css/styles.css';
+var topActionsFilters = document.querySelector("#top-actions-filters");
+var moreLessTrigger = document.querySelector("#more-less-trigger");
+moreLessTrigger.addEventListener("click", toggleTopActionsFilters);
+
+function toggleTopActionsFilters() {
+  if (topActionsFilters.classList.contains("more")) {
+    topActionsFilters.classList.remove("more");
+    moreLessTrigger.classList.remove("more");
+  } else {
+    topActionsFilters.classList.add("more");
+    moreLessTrigger.classList.add("more");
+  }
+}
 
 /***/ }),
 
--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 // import '../assets/css/styles.css';
+
+const topActionsFilters = document.querySelector("#top-actions-filters");
+const moreLessTrigger = document.querySelector("#more-less-trigger");
+moreLessTrigger.addEventListener("click", toggleTopActionsFilters);
+
+function toggleTopActionsFilters() {
+  if (topActionsFilters.classList.contains("more")) {
+    topActionsFilters.classList.remove("more");
+    moreLessTrigger.classList.remove("more");
+  } else {
+    topActionsFilters.classList.add("more");
+    moreLessTrigger.classList.add("more");
+  }
+}
